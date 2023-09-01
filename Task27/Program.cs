f27806cb03b76170df7d5263da72b9ef3456e678
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-01 17:53:33 +0300

    input numABC and output sum A B C

---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumPositionNum(int numA)
+{
+    int SumP = default;
+    string check = numA.ToString();
+    for (int i = 0; i < check.Length; i++)
+    {
+        int result = numA % 10;
+        SumP += result;
+        numA = numA / 10;
+    
+    }
+
+    return SumP;
+}
+
+int SumPosition = SumPositionNum(9012);
+System.Console.WriteLine(SumPosition);
