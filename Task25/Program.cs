7040830b5c1d4aaa1e5bf07ae20533d73386f381
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-01 16:52:13 +0300

    outpoot result to num A exponention num B

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+
+int NumAExponNumB(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        result *= numA;
+    }
+
+    return result;
+}
+
+int Expon = NumAExponNumB(2, 4);
+System.Console.WriteLine(Expon);
